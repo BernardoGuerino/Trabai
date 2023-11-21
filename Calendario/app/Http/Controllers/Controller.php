493aff4701d1cac1c1215cb7d802695ed8096cbf
Dann <dannasser2007@gmail.com>
2023-11-21 20:39:32 -0300

    .

--- Calendario/app/Http/Controllers/Controller.php
+++ Calendario/app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use app\models\cliente;
+use app\models\tarefa;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
