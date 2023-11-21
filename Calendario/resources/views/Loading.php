2e53d8b786b7b7b16f9903730d54f1eadf89000e
BernardoGuerino <bernardo.coelho.guerino@gmail.com>
2023-11-20 21:08:09 -0300

    ?????

---
+++ Calendario/resources/views/Loading.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loading</title>
+    <style>
+        .loanding{
+            width: ;
+        }
+    </style>
+</head>
+<body>
+    <div class="loading">
+  
+</div>
+    
+</body>
+</html>
+    
